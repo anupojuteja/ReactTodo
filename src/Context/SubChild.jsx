d9b86f454bac50fde17f0e8b62ac206cab7453ef
anupojuteja <anupojunagamaniteja@gmail.com>
2025-04-26 16:36:20 +0530

    use context edit

--- src/Context/SubChild.jsx
+++ src/Context/SubChild.jsx
@@ -1,15 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MyContext } from "./Parent";
 
 export default function SubChild() {
-  const items = useContext(MyContext);
+  const { data, setData } = useContext(MyContext);
+  const [editIndex, setEditIndex] = useState(null);
+  const [newValue, setNewValue] = useState("");
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setNewValue(data[index]);
+  };
+
+  const handleSave = () => {
+    const updatedData = [...data];
+    updatedData[editIndex] = newValue;
+    setData(updatedData);
+    setEditIndex(null);
+    setNewValue("");
+  };
 
   return (
     <div>
-      <h6>SubChild Component</h6>
+      <h6>SubChild Component (Editable)</h6>
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
+        {data.map((item, index) => (
+          <li key={index}>
+            {editIndex === index ? (
+              <>
+                <input
+                  type="text"
+                  value={newValue}
+                  onChange={(e) => setNewValue(e.target.value)}
+                />
+                <button className="btn btn-success btn-sm ms-2" onClick={handleSave}>Save</button>
+              </>
+            ) : (
+              <>
+                {item}
+                <button className="btn btn-warning btn-sm ms-2" onClick={() => handleEdit(index)}>Edit</button>
+              </>
+            )}
+          </li>
         ))}
       </ul>
     </div>
